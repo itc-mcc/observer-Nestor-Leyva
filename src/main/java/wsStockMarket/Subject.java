3fb798636712a410905caef880b07d3ccd4c773d
Nestor-Leyva <75275108+Nestor-Leyva@users.noreply.github.com>
2021-04-19 12:15:34 -0600

    Update Subject.java

--- src/main/java/wsStockMarket/Subject.java
+++ src/main/java/wsStockMarket/Subject.java
@@ -1,5 +1,7 @@
 package wsStockMarket;
 
-public interface Subject {
-
+public interface Subject
+{
+    void registrarObs(Observer obs);
+    String notificarObs(Observer obst, String transType, Double precio);
 }
