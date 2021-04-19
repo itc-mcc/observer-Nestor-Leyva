e072304d49862e61abdef18079c76a71dd1d08e9
Nestor-Leyva <75275108+Nestor-Leyva@users.noreply.github.com>
2021-04-19 12:17:34 -0600

    Update Trader.java

--- src/main/java/wsStockMarket/Trader.java
+++ src/main/java/wsStockMarket/Trader.java
@@ -1,8 +1,26 @@
 package wsStockMarket;
+import java.util.*;
 
-public class Trader implements Observer {
+public class Trader implements Observer
+{
+	String name;
+	Map<Stock,Double> stonks;
 
-	public Trader(String nombre, StockMarket mercado_acciones) {
+	public Trader(String nombre, StockMarket mercado_acciones)
+	{
+		stonks = new HashMap<>();
+		name = nombre;
 	}
-    
+
+	public String getName() {return this.name;}
+
+	public void update(Subject suj, String trans, Double precio)
+	{
+		if (suj instanceof Stock)
+		{
+			Stock stonk = (Stock) suj;
+			if (trans.equals("buy")) {this.stonks.put(stonk, precio);}
+			if (trans.equals("sell")) {this.stonks.remove(stonk);}
+		}
+	}
 }
